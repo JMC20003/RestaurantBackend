9e212532a17a7fae40a514471e2e93397463b6a0
JMC20003 <u202123541@gmail.com>
2025-09-27 16:11:27 -0500

    crendo servide de unidad de medida

--- src/main/java/com/bittecsoluciones/restaurantepos/Service/UnitMeasureService.java
+++ src/main/java/com/bittecsoluciones/restaurantepos/Service/UnitMeasureService.java
@@ -1,4 +1,16 @@
 package com.bittecsoluciones.restaurantepos.Service;
 
-public interface UnitMeasureService {
+import com.bittecsoluciones.restaurantepos.Entity.UnitMeasure;
+import com.bittecsoluciones.restaurantepos.Entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UnitMeasureService  {
+    List<UnitMeasure> getAllUnitMeasures();
+    Optional<UnitMeasure> getUnitMeasureById(Long id);
+    UnitMeasure createUnitMeasure(UnitMeasure unitMeasure);
+    UnitMeasure updateUnitMeasure(Long id, UnitMeasure unitMeasure);
+    void deleteUnitMeasure(Long id);
+    Optional<UnitMeasure> findByName(String name);
 }
